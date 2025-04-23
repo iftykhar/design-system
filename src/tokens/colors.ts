d10ea2fe2f2b00a273f193d9268f1dbbf821bc7a
iftykhar alam <s.m.ifty49@gmail.com>
2025-04-24 01:55:18 +0600

    color and typography

---
+++ src/tokens/colors.ts
@@ -0,0 +1,8 @@
+const colors = {
+    primary: '#0070f3',
+    secondary: '#1c1c1e',
+    // Add other colors here
+  };
+  
+  export default colors;
+  
---
+++ src/tokens/typography.ts
@@ -0,0 +1,13 @@
+export const typography = {
+    fontSizes: {
+      h1: '2.25rem',
+      h2: '1.875rem',
+      h3: '1.5rem',
+      // ...
+    },
+    fontWeights: {
+      bold: 700,
+      normal: 400,
+    },
+  };
+  
